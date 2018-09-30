7468096bfca27693029640b9d836e2bce23d9172
Jack Yang <ja199102@gmail.com>
2018-09-30 11:39:40 +0800

    second

--- guess-number.py
+++ guess-number.py
@@ -1,10 +1,13 @@
 import random
 r = random.randint(1, 100)
+i = 0
 while True:
+	i += 1  #等於 i = i + 1
 	num = input('請猜數字: ')
 	num = int(num)
+	print('進行第', i, '次猜測')
 	if num == r:
-		print('終於猜對了!')
+		print('猜對了!')
 		break
 	else:
 		print('猜錯了!')
